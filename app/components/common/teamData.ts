f3e41f09998b4874a0da97e6455ac5cca1508a97
Arigbo Jesse <arigbojesse75@gmail.com>
2026-09-23 21:25:10 +0100

    fixesq for SEO

--- app/components/common/teamData.ts
+++ app/components/common/teamData.ts
@@ -2,5 +2,6 @@
  * Re-exports from the canonical data source at lib/data/teamData.ts.
  * Import from there directly for new code.
  */
+
 export type { TeamMember } from "@/lib/data/teamData";
 export { defaultTeam } from "@/lib/data/teamData";
